c3766c37f00a871a06db035de2303b7b7495f639
Siddhant Raj <1602125@kiit.ac.in>
2023-11-18 14:41:42 +0530

    new counter state added

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -42,6 +42,8 @@ function DateCounter() {
     const initialState = { count: 0, step: 1 };
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const [mysyaye, setmystate] = useState(0);
+
     const { count, step } = state;
 
     // This mutates the date object.
